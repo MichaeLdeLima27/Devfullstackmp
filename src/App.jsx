8617ec12c7a85753e4a87131e6a7aedc06408b77
MichelPianaro <teconhecer27@gmail.com>
2025-10-14 20:58:37 -0300

    resolvendo bug no build

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Navbar from './pages/Home/Navbar.jsx';
-import Home from './pages/Home/Homescreen/index.jsx';
+import Navbar from './pages/home/Navbar.jsx';
+import Home from './pages/home/homescreen/index.jsx';
 
 function App() {
   return (
