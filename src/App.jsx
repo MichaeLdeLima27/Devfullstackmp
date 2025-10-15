f57c870e2e4860d56350de09de2302baa910d0a2
MichelPianaro <teconhecer27@gmail.com>
2025-10-14 21:11:06 -0300

    fix: corrigido import do Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Navbar from './pages/home/Navbar.jsx';
+import Navbar from "./pages/home/Navbar.jsx";
 import Home from './pages/home/homescreen/index.jsx';
 
 function App() {
