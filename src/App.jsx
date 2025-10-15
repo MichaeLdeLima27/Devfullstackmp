a7bdfb9d936ee3cc25ae266017e7338dfa40e2fd
MichelPianaro <teconhecer27@gmail.com>
2025-10-14 21:50:24 -0300

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from "./Pages/Home/Navbar.jsx";
-import Home from './pages/Home/Homescreen/index.jsx';
+import Home from './pages/Home/homescreen/index.jsx';
 
 function App() {
   return (
